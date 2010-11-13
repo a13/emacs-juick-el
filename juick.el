a08a081ab50f79ed95f3f50121e2ee9ffe26d697
Dmitry K <lazyklimm@gmail.com>
2010-11-13 16:12:32 +0300

    bla-bla

--- juick.el
+++ juick.el
@@ -186,7 +186,7 @@ Use FORCE to markup any buffer"
 (defun juick-delimiter-insert ()
   (goto-char (or juick-point-last-message (point-min)))
   (let ((inhibit-read-only t))
-    (while (re-search-forward "\\(> @\\|^@\\)\\([0-9A-Za-z@\\.\\-]+\\):" nil t)
+    (while (re-search-forward "\\(Reply by @\\|> @\\|^@\\)\\([0-9A-Za-z@\\.\\-]+\\):" nil t)
       (goto-char (match-beginning 0))
       (re-search-forward "@" nil t)
       (goto-char (- (point) 1))
@@ -468,6 +468,11 @@ in a match, if match send fake message himself"
   (juick-send-message juick-bot-jid
 		      (concat "! " (match-string-no-properties 0))))
 
+(define-juick-action juick-like
+  (thing-at-point-looking-at juick-username-simple-regex)
+  (juick-send-message juick-bot-jid
+		      (concat "L " (match-string-no-properties 0))))
+
 (define-juick-action juick-list-messages
   (or (thing-at-point-looking-at juick-id-simple-regex)
       (thing-at-point-looking-at juick-username-simple-regex))
@@ -476,8 +481,11 @@ in a match, if match send fake message himself"
 				  (match-string-no-properties 1)
 				(match-string-no-properties 0))
 			      "+")))
+
 (define-key jabber-chat-mode-map "!" 'juick-recommend)
 (define-key jabber-chat-mode-map "+" 'juick-list-messages)
+(define-key jabber-chat-mode-map "l" 'juick-like)
+(define-key jabber-chat-mode-map "д" 'juick-like)
 
 (defun juick-remove-tag ()
   (interactive)
